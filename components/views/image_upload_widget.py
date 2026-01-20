0a79ef0358796f46bbab30a1d5b32902e974799a
rgn2836 <bublik0706@gmail.com>
2026-01-20 20:00:26 +0300

    Рефакторинг компонента ImageUploadWidgetComponent по паттерну Page Factory

--- components/views/image_upload_widget.py
+++ components/views/image_upload_widget.py
@@ -2,6 +2,11 @@ from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
 from components.views.empty_view_component import EmptyViewComponent
+from elements.image import Image
+from elements.icon import Icon
+from elements.text import Text
+from elements.button import Button
+from elements.file import FileInput
 
 class ImageUploadWidgetComponent(BaseComponent):
     def __init__(self, page: Page, identifier):
@@ -9,15 +14,15 @@ class ImageUploadWidgetComponent(BaseComponent):
 
         self.preview_empty_view = EmptyViewComponent(page, identifier)
 
-        self.preview_image = self.page.get_by_test_id(f'{identifier}-image-upload-widget-preview-image')
+        self.preview_image = Image(page, f'{identifier}-image-upload-widget-preview-image', 'Image')
 
-        self.image_upload_info_icon = self.page.get_by_test_id(f'{identifier}image-upload-widget-info-icon')
-        self.image_upload_info_tittle = self.page.get_by_test_id(f'{identifier}-image-upload-widget-info-title-text')
-        self.image_upload_info_description = self.page.get_by_test_id(f'{identifier}-image-upload-widget-info-description-text')
+        self.image_upload_info_icon = Icon(page, f'{identifier}image-upload-widget-info-icon', 'Icon')
+        self.image_upload_info_tittle = Text(page, f'{identifier}-image-upload-widget-info-title-text', 'Tittle')
+        self.image_upload_info_description = Text(page, f'{identifier}-image-upload-widget-info-description-text', 'Description')
 
-        self.upload_image_button = self.page.get_by_test_id(f'{identifier}-image-upload-widget-upload-button')
-        self.upload_image_input = self.page.get_by_test_id(f'{identifier}image-upload-widget-input')
-        self.remove_button = self.page.get_by_test_id(f'{identifier}-image-upload-widget-remove-button')
+        self.upload_image_button = Button(page, f'{identifier}-image-upload-widget-upload-button', 'Upload image')
+        self.upload_image_input = FileInput(page, f'{identifier}image-upload-widget-input', 'Upload input')
+        self.remove_button = Button(page, f'{identifier}-image-upload-widget-remove-button', 'Delete image')
 
     def click_remove_image(self):
         self.remove_button.click()
@@ -26,17 +31,17 @@ class ImageUploadWidgetComponent(BaseComponent):
         self.upload_image_input.set_input_files(file)
 
     def check_visible(self, is_image_uploaded: bool = False):
-        expect(self.image_upload_info_description).to_be_visible()
-        expect(self.image_upload_info_description).to_have_text('Recommended file size 540X300')
+        self.image_upload_info_description.check_visible()
+        self.image_upload_info_description.check_have_text('Recommended file size 540X300')
 
-        expect(self.image_upload_info_tittle).to_be_visible()
-        expect(self.image_upload_info_tittle).to_have_text('Tap on "Upload image" button to select file')
+        self.image_upload_info_tittle.check_visible()
+        self.image_upload_info_tittle.check_have_text('Tap on "Upload image" button to select file')
 
-        expect(self.image_upload_info_icon).to_be_visible()
+        self.image_upload_info_icon.check_visible()
 
         if is_image_uploaded:
-            expect(self.preview_image).to_be_visible()
-            expect(self.remove_button).to_be_visible()
+            self.preview_image.check_visible()
+            self.remove_button.check_visible()
 
         else:
             self.preview_empty_view.check_visible(
