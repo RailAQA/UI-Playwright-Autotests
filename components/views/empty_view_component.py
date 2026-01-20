467fd8591a9a1ebb863c08f1480671a2c19204d2
rgn2836 <bublik0706@gmail.com>
2026-01-20 19:51:54 +0300

    Рефакторинг компонента EmptyViewComponent по паттерну Page Factory

--- components/views/empty_view_component.py
+++ components/views/empty_view_component.py
@@ -1,20 +1,22 @@
 from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
+from elements.text import Text
+from elements.icon import Icon
 
 class EmptyViewComponent(BaseComponent):
     def __init__(self, page: Page, identifier):
         super().__init__(page)
 
-        self.icon = self.page.get_by_test_id(f'{identifier}-empty-view-icon')
-        self.tittle = self.page.get_by_test_id(f'{identifier}-empty-view-title-text')
-        self.description = self.page.get_by_test_id(f'{identifier}-empty-view-description-text')
+        self.icon = Icon(page, f'{identifier}-empty-view-icon', 'Icon')
+        self.tittle = Text(page, f'{identifier}-empty-view-title-text', 'Tittle')
+        self.description = Text(page, f'{identifier}-empty-view-description-text', 'Description')
 
     def check_visible(self, tittle: str, description: str):
-        expect(self.icon).to_be_visible()
+        self.icon.check_visible()
 
-        expect(self.tittle).to_be_visible()
-        expect(self.tittle).to_have_text(tittle)
+        self.tittle.check_visible()
+        self.tittle.check_have_text(tittle)
 
-        expect(self.description).to_be_visible()
-        expect(self.description).to_have_text(description)
+        self.description.check_visible()
+        self.description.check_have_text(description)
