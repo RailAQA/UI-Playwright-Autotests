d3bb592bd16cf2ccbcfdfc35b1a601e5203fcdcb
rgn2836 <bublik0706@gmail.com>
2026-01-20 19:42:19 +0300

    Рефакторинг компонента NavbarComponent по паттерну page factory

--- components/navigation/navbar_component.py
+++ components/navigation/navbar_component.py
@@ -1,17 +1,18 @@
 from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
+from elements.text import Text
 
 class NavbarComponent(BaseComponent):
     def __init__(self, page):
         super().__init__(page)
 
-        self.navbar_title = self.page.get_by_test_id('navigation-navbar-app-title-text')
-        self.welcome_tittle = self.page.get_by_test_id('navigation-navbar-welcome-title-text')
+        self.navbar_title = Text(page, 'navigation-navbar-app-title-text', 'Navbar tittle')
+        self.welcome_tittle = Text(page, 'navigation-navbar-welcome-title-text', 'Welcome tittle')
 
     def check_visible(self, username: str):
-        expect(self.navbar_title).to_be_visible()
-        expect(self.navbar_title).to_have_text('UI Course')
+        self.navbar_title.check_visible()
+        self.navbar_title.check_have_text('UI Course')
 
-        expect(self.welcome_tittle).to_be_visible()
-        expect(self.welcome_tittle).to_have_text(f'Welcome, {username}!')
+        self.welcome_tittle.check_visible()
+        self.welcome_tittle.check_have_text(f'Welcome, {username}!')
