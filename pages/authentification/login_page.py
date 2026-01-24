425a3021fdfdb66d43a9a44913e9cd2364237aad
rgn2836 <bublik0706@gmail.com>
2026-01-24 14:48:13 +0300

    Написан параметрайз негативный тест на авторизацию

--- pages/authentification/login_page.py
+++ pages/authentification/login_page.py
@@ -4,16 +4,18 @@ import re
 
 from components.authentication.login_form_component import LoginFormComponent
 from components.authentication.registration_form_component import RegistrationFormComponent
-from elements.button import Button
+from elements.text import Text
 
 
 class LoginPage(BasePage):
-    def __init__(self, page):
+    def __init__(self, page: Page):
         super().__init__(page)
 
         self.login_form = LoginFormComponent(page)
         self.login_button = LoginFormComponent(page).login_button
         self.registration_button = LoginFormComponent(page).registation_button
+
+        self.wrong_email_or_password_alert = Text(page, 'login-page-wrong-email-or-password-alert', 'Alert')
 
     def click_login_button(self):
         self.login_button.click()
@@ -22,3 +24,7 @@ class LoginPage(BasePage):
     def click_registration_link(self):
         self.registration_button.click()
         self.check_current_url(re.compile(r'.*/#/auth/registration'))
+
+    def check_visible_wrong_email_or_password(self):
+        self.wrong_email_or_password_alert.check_visible()
+        self.wrong_email_or_password_alert.check_have_text('Wrong email or password')
---
+++ tests/authentification/test_login.py
@@ -0,0 +1,18 @@
+import pytest
+from playwright.sync_api import Page, Playwright
+
+from pages.authentification.login_page import LoginPage
+
+class TestLogin:
+    @pytest.mark.parametrize('email, password', 
+                             [
+                                 ('user.name@mail.ru', 'password'),
+                                 ('user@yandex.ru', '   '),
+                                 ('   ', 'password')
+                             ]
+                             )
+    def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
+        login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+        login_page.login_form.fill(email, password)
+        login_page.login_button.click()
+        login_page.check_visible_wrong_email_or_password()
