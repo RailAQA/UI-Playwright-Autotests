060ba5a5dac280415803c2d5bfb83bc835b5741b
rgn2836 <bublik0706@gmail.com>
2026-01-29 21:15:40 +0300

    Добавлена запись видео тестов для алюр отчета

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -8,16 +8,18 @@ from pages.authentification.registration_page import RegistrationPage
 @pytest.fixture()
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
     page = playwright.chromium.launch(headless=False)
-    context = page.new_context()
+    context = page.new_context(record_video_dir='./videos')
     context.tracing.start(
         screenshots=True, 
         snapshots=True, 
         sources=True
         )
-    yield page.new_page()
+    page = context.new_page()
+    yield page
     context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
     page.close()
     allure.attach.file(source=f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    allure.attach.file(source=page.video.path(), name='video', extension=allure.attachment_type.WEBM)
 
 @pytest.fixture(scope='session')
 def initialize_browser_state(playwright: Playwright):
@@ -35,8 +37,11 @@ def initialize_browser_state(playwright: Playwright):
 @pytest.fixture
 def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser_state.json")
+    context = browser.new_context(storage_state="browser_state.json", record_video_dir='./videos')
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    yield context.new_page()
+    page = context.new_page()
+    yield page
     context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
     browser.close()
+    allure.attach.file(source=f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    allure.attach.file(source=page.video.path(), name='video', extension=allure.attachment_type.WEBM)
